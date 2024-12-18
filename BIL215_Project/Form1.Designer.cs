f6e573acddb29c515a05be719eb0f5d311d30edb
Eren Kaplan <ek.xcodeproject@gmail.com>
2024-12-19 00:03:00 +0300

    Form dizaynına verilerin listelenmesi için DataGridView eklendi. Ürün eklemek ve silmek için textbox ve button'lar oluşturuldu

--- BIL215_Project/Form1.Designer.cs
+++ BIL215_Project/Form1.Designer.cs
@@ -28,20 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnUrunSil = new System.Windows.Forms.Button();
+            this.txUrunSil = new System.Windows.Forms.TextBox();
+            this.txUrunFiyati = new System.Windows.Forms.TextBox();
+            this.txUrunIsmi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUrunEkle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.itemNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.basePriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuItemsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bIL215_ProjeDataSet = new BIL215_Project.BIL215_ProjeDataSet();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.menuItemsTableAdapter = new BIL215_Project.BIL215_ProjeDataSetTableAdapters.MenuItemsTableAdapter();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuItemsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bIL215_ProjeDataSet)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(963, 434);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnUrunSil);
+            this.tabPage1.Controls.Add(this.txUrunSil);
+            this.tabPage1.Controls.Add(this.txUrunFiyati);
+            this.tabPage1.Controls.Add(this.txUrunIsmi);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnUrunEkle);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(955, 405);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnUrunSil
+            // 
+            this.btnUrunSil.Location = new System.Drawing.Point(611, 331);
+            this.btnUrunSil.Name = "btnUrunSil";
+            this.btnUrunSil.Size = new System.Drawing.Size(75, 23);
+            this.btnUrunSil.TabIndex = 7;
+            this.btnUrunSil.Text = "Ürün Sil";
+            this.btnUrunSil.UseVisualStyleBackColor = true;
+            this.btnUrunSil.Click += new System.EventHandler(this.btnUrunSil_Click_1);
+            // 
+            // txUrunSil
+            // 
+            this.txUrunSil.Location = new System.Drawing.Point(437, 331);
+            this.txUrunSil.Name = "txUrunSil";
+            this.txUrunSil.Size = new System.Drawing.Size(156, 22);
+            this.txUrunSil.TabIndex = 6;
+            // 
+            // txUrunFiyati
+            // 
+            this.txUrunFiyati.Location = new System.Drawing.Point(437, 60);
+            this.txUrunFiyati.Name = "txUrunFiyati";
+            this.txUrunFiyati.Size = new System.Drawing.Size(156, 22);
+            this.txUrunFiyati.TabIndex = 5;
+            // 
+            // txUrunIsmi
+            // 
+            this.txUrunIsmi.Location = new System.Drawing.Point(437, 24);
+            this.txUrunIsmi.Name = "txUrunIsmi";
+            this.txUrunIsmi.Size = new System.Drawing.Size(156, 22);
+            this.txUrunIsmi.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ürünün Fiyatı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(344, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ürünün İsmi";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnUrunEkle
+            // 
+            this.btnUrunEkle.Location = new System.Drawing.Point(611, 60);
+            this.btnUrunEkle.Name = "btnUrunEkle";
+            this.btnUrunEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnUrunEkle.TabIndex = 1;
+            this.btnUrunEkle.Text = "Ürün Ekle";
+            this.btnUrunEkle.UseVisualStyleBackColor = true;
+            this.btnUrunEkle.Click += new System.EventHandler(this.btnUrunEkle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.itemNameDataGridViewTextBoxColumn,
+            this.basePriceDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.menuItemsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(332, 393);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // itemNameDataGridViewTextBoxColumn
+            // 
+            this.itemNameDataGridViewTextBoxColumn.DataPropertyName = "ItemName";
+            this.itemNameDataGridViewTextBoxColumn.HeaderText = "ItemName";
+            this.itemNameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.itemNameDataGridViewTextBoxColumn.Name = "itemNameDataGridViewTextBoxColumn";
+            this.itemNameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // basePriceDataGridViewTextBoxColumn
+            // 
+            this.basePriceDataGridViewTextBoxColumn.DataPropertyName = "BasePrice";
+            this.basePriceDataGridViewTextBoxColumn.HeaderText = "BasePrice";
+            this.basePriceDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.basePriceDataGridViewTextBoxColumn.Name = "basePriceDataGridViewTextBoxColumn";
+            this.basePriceDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // menuItemsBindingSource
+            // 
+            this.menuItemsBindingSource.DataMember = "MenuItems";
+            this.menuItemsBindingSource.DataSource = this.bIL215_ProjeDataSet;
+            // 
+            // bIL215_ProjeDataSet
+            // 
+            this.bIL215_ProjeDataSet.DataSetName = "BIL215_ProjeDataSet";
+            this.bIL215_ProjeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(955, 405);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(955, 405);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // menuItemsTableAdapter
+            // 
+            this.menuItemsTableAdapter.ClearBeforeFill = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(994, 466);
+            this.ClientSize = new System.Drawing.Size(987, 458);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuItemsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bIL215_ProjeDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private BIL215_ProjeDataSet bIL215_ProjeDataSet;
+        private System.Windows.Forms.BindingSource menuItemsBindingSource;
+        private BIL215_ProjeDataSetTableAdapters.MenuItemsTableAdapter menuItemsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itemNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn basePriceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox txUrunIsmi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUrunEkle;
+        private System.Windows.Forms.TextBox txUrunFiyati;
+        private System.Windows.Forms.Button btnUrunSil;
+        private System.Windows.Forms.TextBox txUrunSil;
     }
 }
 
--- BIL215_Project/Form1.cs
+++ BIL215_Project/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,62 @@ namespace BIL215_Project
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'bIL215_ProjeDataSet.MenuItems' table. You can move, or remove it, as needed.
+            this.menuItemsTableAdapter.Fill(this.bIL215_ProjeDataSet.MenuItems);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUrunEkle_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=LAPTOP\\SQLEXPRESS;Database=BIL215_Proje;Trusted_Connection=True;";
+            string urunIsmi = txUrunIsmi.Text;
+            string urunFiyat = txUrunFiyati.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO MenuItems (ItemName, BasePrice) VALUES (@ItemName, @BasePrice)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ItemName", urunIsmi);
+                    command.Parameters.AddWithValue("@BasePrice", urunFiyat);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+
+            MessageBox.Show("Ürün başarıyla kaydedildi.");
+        }
+
+        private void btnUrunSil_Click_1(object sender, EventArgs e)
+        {
+            string connectionString = "Server=LAPTOP\\SQLEXPRESS;Database=BIL215_Proje;Trusted_Connection=True;";
+            string urunSil = txUrunSil.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM MenuItems WHERE ItemName = @ItemName";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ItemName", urunSil);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+
+            MessageBox.Show("Ürün başarıyla silindi.");
         }
     }
 }
